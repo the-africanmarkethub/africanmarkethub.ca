19015d2be4bc351e8ec0371d9ee92d0d4720f9ac
Afolabi Marcus <afolabi.marcus@pawait.co.ke>
2025-10-17 10:55:15 +0300

    WIP

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -1218,6 +1218,7 @@
 
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                     {{env('APP_NAME', 'Laravel')}}
+                    
                 </footer>
             </div>
         </div>
