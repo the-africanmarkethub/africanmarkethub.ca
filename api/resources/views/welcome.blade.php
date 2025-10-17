ddcc159dfcabb22438e43b964609899ed17f0ee3
Afolabi Marcus <afolabi.marcus@pawait.co.ke>
2025-10-17 10:39:10 +0300

    WIP

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -1215,9 +1215,10 @@
         <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
         <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
- 
+
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                     {{env('APP_NAME', 'Laravel')}}
+                    
                 </footer>
             </div>
         </div>
