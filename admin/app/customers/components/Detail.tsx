0ffa94799002432e0f4f40dd78564407a90cc87f
Afolabi Marcus <afolabi.marcus@pawait.co.ke>
2025-10-17 13:46:44 +0300

    restructure the repo to fit branch

--- admin/app/customers/components/Detail.tsx
+++
@@ -1,8 +0,0 @@
-export default function Detail() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">Users</h1>
-            <p>Manage your users here.</p>
-        </div>
-    );
-}
--- api/config/canadapost.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'username' => env('CANADA_USERNAME'),
-    'password'  => env('CANADA_PASSWORD'),
-    'customer_number'        => env('CANADA_CUSTOMER_NUMBER'),
-];
