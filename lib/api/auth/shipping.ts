8c502a8d79c47d0cb62457db6983676a2ad6b02f
biormarcus <biormarcus@gmail.com>
2025-12-19 01:25:30 +0300

    removed the customer route since the address is needed by both seller and customer

--- lib/api/auth/shipping.ts
+++ lib/api/auth/shipping.ts
@@ -1,14 +1,13 @@
-// lib/api/auth/shipping.ts
 import Address from "@/interfaces/address";
 import api from "../axios";
  
 
 export async function updateAddress(payload: Address): Promise<Address> {
-  const response = await api.post(`/customer/address/upsert`, payload);
+  const response = await api.post(`/address/upsert`, payload);
   return response.data.data;
 }
 
 export async function getAddress(): Promise<Address> {
-  const response = await api.get(`/customer/addresses`); 
+  const response = await api.get(`/addresses`); 
   return response.data.data;
 }
