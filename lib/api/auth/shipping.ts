c6c6ebe6da2633b1155a3b907c6ba938686488c5
MarcusAfolabi <biormarcus@gmail.com>
2026-02-10 01:43:59 +0300

    updated

--- lib/api/auth/shipping.ts
+++ lib/api/auth/shipping.ts
@@ -3,11 +3,11 @@ import api from "../axios";
  
 
 export async function updateAddress(payload: Address): Promise<Address> {
-  const response = await api.post(`/address/upsert`, payload);
+  const response = await api.post(`/profile/address/save`, payload);
   return response.data.data;
 }
 
 export async function getAddress(): Promise<Address> {
-  const response = await api.get(`/addresses`); 
+  const response = await api.get(`/profile/address`); 
   return response.data.data;
 }
