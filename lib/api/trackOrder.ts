73c7c25672d4cadb3c0caadedf0b1f7719292d75
MarcusAfolabi <biormarcus@gmail.com>
2026-03-30 00:40:18 +0300

    cleanup

--- lib/api/trackOrder.ts
+++ lib/api/trackOrder.ts
@@ -3,9 +3,10 @@ import api from "./axios";
 /**
  * Tracks orders associated with a specific email.
  */
-export const trackOrder = async (email: string) => {
+export const trackOrder = async (email: string, orderId?: string) => {
   const res = await api.post("order/track", {
-    email, 
+    email,
+    order_id: orderId, // This will be sent as undefined if not provided, which is fine
   });
   return res.data;
 };
